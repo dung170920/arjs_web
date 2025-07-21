f29596d3b40d2e93b8485892db0733ca46365110
dung170920 <dungnguyen09172000@gmail.com>
2025-07-21 13:07:40 +0700

    fix distance v5

--- src/App.tsx
+++ src/App.tsx
@@ -141,8 +141,8 @@ const App: React.FC = () => {
         const rad = THREE.MathUtils.degToRad(item.heading);
 
         const distanceMax = Math.max(...dataItems.map(item => item.distance));
-        const radiusMin = 500;
-        const radiusMax = 2000;
+        const radiusMin = 100;
+        const radiusMax = 1000;
         const normalized = item.distance / distanceMax;
         const radius = radiusMin + normalized * (radiusMax - radiusMin);
 
