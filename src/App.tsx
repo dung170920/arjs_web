c090e1157a2b2af9e5f7bc8a66a84ac1834ecd51
dung170920 <dungnguyen09172000@gmail.com>
2025-07-22 19:36:50 +0700

    fix câulate v3

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ const App: React.FC = () => {
       const dir = new THREE.Vector3();
       camera.getWorldDirection(dir);
 
-      const angleRad = Math.atan2(-dir.x, -dir.z);
+      const angleRad = Math.atan2(dir.x, -dir.z);
       const angleDeg = ((THREE.MathUtils.radToDeg(angleRad) + 360) % 360);
 
       if (initialHeadingRef.current === null) {
